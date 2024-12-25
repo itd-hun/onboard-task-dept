31952c7bc7d468a1322b7f336371af61a4e5933e
itd-hun <harikaran.unnikrishnan@itdesign.de>
2024-12-25 05:15:17 +0000

    Added CSV file read

--- src/main/groovy/org/onboard/Main.groovy
+++ src/main/groovy/org/onboard/Main.groovy
@@ -1,5 +1,33 @@
 package org.onboard
 
 static void main(String[] args) {
-    println "Hello world!"
+
+    def resource = getClass().getClassLoader().getResource("departments.csv")
+    def filePath = resource ? resource.toURI().path : null
+
+    String line = ""
+    BufferedReader br = null;
+    def department = [:]
+
+    //file read
+    try {
+        br = new BufferedReader(new FileReader(filePath))
+        List<List<String>> deptList = new ArrayList<>();
+        while((line = br.readLine()) != null){
+            List<String> values = line.split(",")
+            if(values[0] != "#"){
+                values.remove(0)
+                deptList.add(values)
+            }
+        }
+        deptList.remove(0)
+        println(deptList)
+    }catch (Exception e){
+        e.printStackTrace()
+    }finally {
+        if(br != null){
+            br.close()
+        }
+    }
+
 }
