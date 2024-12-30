d221c50b403cdc616807382ff7632612ee791221
itd-hun <harikaran.unnikrishnan@itdesign.de>
2024-12-30 12:14:47 +0000

    Added logs

--- src/main/groovy/org/onboard/Main.groovy
+++ src/main/groovy/org/onboard/Main.groovy
@@ -2,26 +2,46 @@ package org.onboard
 
 import groovy.xml.MarkupBuilder
 import groovy.xml.XmlUtil
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.onboard.Model.Department
+
 
 class CsvToXml {
 
+    private static final Logger logger = LogManager.getLogger(CsvToXml)
+
     static void main(String[] args) {
+
+        logger.info("Application started")
+
         //Load CSV File from Resource
         def resource = CsvToXml.class.classLoader.getResource("departments.csv")
         def filePath = resource ? resource.toURI().path : null
+
+        if (!filePath) {
+            logger.fatal("Required resource file is missing. Can't process further")
+
+            throw new RuntimeException("Critical error: 'departments.csv' is required but not found.")
+        }
+
+        logger.info("Required file found, processing further.")
 
         BufferedReader br = null;
 
         //Read CSV File and Handle Exception
         try {
+            logger.debug("Started reading the file")
             br = new BufferedReader(new FileReader(filePath))
             List<String> departmentData = br.readLines()
 
             List<List<String>> deptList = departmentData.findAll { !it.startsWith("#") }.collect { eachLine ->
 
                 if (!eachLine.isEmpty()) {
-                    eachLine.split(",")[1..-1]
+
+                    def line = eachLine.split(",").findAll { it?.trim() }
+                    return line
+//
                 } else {
                     []
                 }
@@ -30,7 +50,11 @@ class CsvToXml {
 
             List<String> fileHeaders = deptList.remove(0)
 
+            logger.info("Successfully converted into base list")
+
             List<List<Department>> hierarchyDeptList = generateHierarchyDept(deptList)
+
+            logger.info("Successfully converted into hierarchy list")
 
             String xmlString = generateXML(hierarchyDeptList)
 
@@ -42,11 +66,20 @@ class CsvToXml {
             file.parentFile.mkdirs()
             file.write(formattedXml)
 
+            logger.debug("Successfully generated XML file from CSV")
+
+        } catch (FileNotFoundException e) {
+            logger.error("File not found: ${e.message}")
+        } catch (IOException e) {
+            logger.error("I/O Error: ${e.message}")
+        } catch (SecurityException e) {
+            logger.error("Security exception: ${e.message}")
         } catch (Exception e) {
-            e.printStackTrace()
+            logger.error("Unexpected error: ${e.message}")
         } finally {
             if (br != null) {
                 br.close()
+                logger.info("Closing the file reader")
             }
         }
 
@@ -84,6 +117,8 @@ class CsvToXml {
         def writer = new StringWriter()
         MarkupBuilder xml = new MarkupBuilder(writer)
 
+        logger.info("Started writing XML")
+
         xml.NikuDataBus('xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
                 'xsi:noNamespaceSchemaLocation': '../xsd/nikuxog_department.xsd') {
 
@@ -101,6 +136,7 @@ class CsvToXml {
 
     //Recursively generates child department xml
     static void generateDepartment(xml, dept) {
+
         xml.Department(department_code: dept.id,
                 dept_manager_code: dept.deptManagerCode,
                 entity: dept.entity,
